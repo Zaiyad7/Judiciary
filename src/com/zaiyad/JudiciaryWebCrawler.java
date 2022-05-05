5b8262c6b8c5ae240e4e45044646e3c78cdcc475
Zaiyad7 <93227269+Zaiyad7@users.noreply.github.com>
2022-05-05 21:16:56 +0100

    Matched older reports and added exit condition

--- src/com/zaiyad/JudiciaryWebCrawler.java
+++ src/com/zaiyad/JudiciaryWebCrawler.java
@@ -16,6 +16,8 @@ public class JudiciaryWebCrawler {
     public String caseDate;
     public String caseText;
     public String titleCheck = "Judgments";
+    public String caseDateOld;
+    public String caseTextOld;
     public org.jsoup.nodes.Document doc;
     public static String url;
 
@@ -27,12 +29,15 @@ public class JudiciaryWebCrawler {
 
     }
     public void judiciaryCrawler() throws InterruptedException {
-        for (int i = 1; i<328; i++) {
-            String pageNum =  (i) + "/?filter_type=judgment";
-            url = "https://www.judiciary.uk/judgments/page/" + pageNum;
-            crawl(1, url, new ArrayList<String>());
 
-        }
+            for (int i = 1; i < 1000; i++) {
+                String pageNum = (i) + "/?filter_type=judgment";
+                url = "https://www.judiciary.uk/judgments/page/" + pageNum;
+                crawl(1, url, new ArrayList<String>());
+
+
+
+            }
 
     }
 
@@ -59,7 +64,6 @@ public class JudiciaryWebCrawler {
         StringBuffer sb = new StringBuffer();
         StringBuffer sb2 = new StringBuffer();
         try {
-
             if (!doc.title().contains(titleCheck)) {
                 for (int i = 4; i < 6; i++) {
                     sb.append(arrOfCourt[i]);
@@ -70,13 +74,14 @@ public class JudiciaryWebCrawler {
                     for (int i = 4; i < arrOfCourt.length; i++) {
                         sb2.append(arrOfCourt[i]);
                     }
-
                 }
                 if (!str.contains("Court")) {
                     System.out.println("Court name not available");
+
                 } else if (str.contains("of")) {
                     String str2 = sb2.toString();
                     System.out.println("Court name: " + str2);
+
                 } else {
                     System.out.println("Court name: " + str);
                 }
@@ -93,19 +98,36 @@ public class JudiciaryWebCrawler {
 
     public void dateMatcher() {
         if (!doc.title().contains(titleCheck)) {
+            Pattern ptD = Pattern.compile("Date(.*)");
+            Matcher mtD = ptD.matcher(caseDate);
             boolean isFound = caseDate.contains("Date");
-            Pattern ptD2 = Pattern.compile("\\d\\d\\s\\w*\\s\\d\\d\\d\\d");
+
+            Pattern ptD2 = Pattern.compile("\\d{1,2}\\s\\w*\\s\\d\\d\\d\\d");
             Matcher mtD2 = ptD2.matcher(caseDate);
             boolean mtD2Found = mtD2.find();
+
+            boolean isOldFound = caseDateOld.contains("Date");
+            Matcher mtDO2 = ptD2.matcher(caseDateOld);
+            boolean mtDO2Found = mtDO2.find();
+
             if (isFound) {
-                Pattern ptD = Pattern.compile("Date(.*)");
-                Matcher mtD = ptD.matcher(caseDate);
                 boolean mtDFound = mtD.find();
                 String date = mtD.group(1);
                 System.out.println("Trial date: " + date);
+
             } else if (mtD2Found) {
                 String dateAlt = mtD2.group();
                 System.out.println("Trial date: " + dateAlt);
+
+            } else if (isOldFound) {
+                Matcher mtDO = ptD.matcher(caseDateOld);
+                boolean mtDOFound = mtDO.find();
+                String dateOld = mtDO.group(1);
+                System.out.println("Trial date: " + dateOld);
+
+            } else if (mtDO2Found) {
+                String dateAltOld = mtDO2.group();
+                System.out.println("Trial date: " + dateAltOld);
 
             } else {
                 System.out.println("Trial date is not available");
@@ -114,33 +136,49 @@ public class JudiciaryWebCrawler {
     }
     public void judgeMatcher() {
         if (!doc.title().contains(titleCheck)) {
-            Pattern ptJ = Pattern.compile("(Before|BEFORE|before)(.*)(Between|BETWEEN|between|B e t w e e n|B E T W E E N)");
+            Pattern ptJ = Pattern.compile("(Before|BEFORE|before)(.*)(Between|BETWEEN|between|B e t w e e n|B E T W E E N|Re)");
             Matcher mtJ = ptJ.matcher(caseText);
             boolean mtJFound = mtJ.find();
+
             Pattern ptJ2 = Pattern.compile("^Between|BETWEEN|B E T W E E N");
             Matcher mtJ2 = ptJ2.matcher(caseText);
             boolean mtJ2Found = mtJ2.find();
+
+            Matcher mtJO = ptJ.matcher(caseTextOld);
+            boolean mtJOFound = mtJO.find();
+
             if (mtJFound) {
                 System.out.println("Judge name " + mtJ.group(2));
+
             } else if (mtJ2Found){
                 System.out.println("Judge name not available");
+
+            } else if (mtJOFound){
+                System.out.println("Judge name " + mtJO.group(2));
+
             } else {
-                System.out.println("Judge/Party info: " + caseText);
+                System.out.println("Judge/Party info: " + caseText + caseTextOld);
             }
         }
 
 
     }
     public void titleMatcher() {
-        if (doc.title().contains(titleCheck)) {
-            String[] arrOfTitle = this.url.split("\\/");
-           System.out.println("Page: " + arrOfTitle[5]);
+        if (!doc.title().contains("Decisions – guidance for Proscribed Organisations Tribunal")) {
+
+            if (doc.title().contains(titleCheck)) {
+                String[] arrOfTitle = this.url.split("\\/");
+                System.out.println("Page: " + arrOfTitle[5]);
+
+            } else {
+                String[] arrOfTitle = doc.title().split("\\|");
+                System.out.println("Title: " + arrOfTitle[0]);
+            }
 
         } else {
-            String [] arrOfTitle = doc.title().split("\\|");
-            System.out.println("Title: " + arrOfTitle[0]);
+            System.out.println("All reports printed");
+            System.exit(0);
         }
-
 
     }
     private  Document request(String url, ArrayList<String> v) {
@@ -153,10 +191,15 @@ public class JudiciaryWebCrawler {
                 Elements pageDate = doc.select("p[style*=right]");
                 Elements pageCourt = doc.select("p:containsOwn(COURT)");
                 Elements courtCheck = doc.select("span > a[href]");
+                Elements dateOld = doc.select("p[align*=right]");
+                Elements pageOld = doc.select("p[align*=center]");
+
                 this.caseCourt = pageCourt.text();
                 this.courtMatch = courtCheck.text();
                 this.caseDate = pageDate.text();
                 this.caseText = page.text();
+                this.caseDateOld = dateOld.text();
+                this.caseTextOld = pageOld.text();
 
                 this.titleMatcher();
                 this.dateMatcher();
